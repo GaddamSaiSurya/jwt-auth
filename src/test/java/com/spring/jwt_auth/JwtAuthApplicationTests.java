7b21850f90c51aa640ab7ac385e41905ad883885
Surya <surya104441@gmail.com>
2026-06-27 11:51:43 +0530

    Add Docker Compose support and production Dockerfile

--- src/test/java/com/spring/jwt_auth/JwtAuthApplicationTests.java
+++ src/test/java/com/spring/jwt_auth/JwtAuthApplicationTests.java
@@ -2,8 +2,10 @@ package com.spring.jwt_auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class JwtAuthApplicationTests {
 
 	@Test
